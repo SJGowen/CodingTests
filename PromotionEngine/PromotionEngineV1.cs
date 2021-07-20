979885c917a68e38253301aa588d9a6aa78b8f69
Stephen Gowen <sjgowen@gmail.com>
2021-07-20 19:21:27 +0100

    Created method Add and CalculateTotal to PromotionEngineV1

--- PromotionEngine/PromotionEngineV1.cs
+++ PromotionEngine/PromotionEngineV1.cs
@@ -4,5 +4,14 @@ namespace PromotionEngine
 {
     public class PromotionEngineV1
     {
+        public void Add(char v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object CalculateTotal()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- PromotionEngineTests/EngineTests.cs
+++ PromotionEngineTests/EngineTests.cs
@@ -9,6 +9,11 @@ namespace PromotionEngineTests
         public void ScenarioA()
         {
             var engine = new PromotionEngineV1();
+            engine.Add('A', 1);
+            engine.Add('B', 1);
+            engine.Add('C', 1);
+            var total = engine.CalculateTotal();
+            Assert.Equal(100, total);
         }
     }
 }
