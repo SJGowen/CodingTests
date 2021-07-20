54d8e724335509d2e724db5a41317fe1a454c882
Stephen Gowen <sjgowen@gmail.com>
2021-07-20 19:18:35 +0100

    Add project files.

---
+++ PromotionEngineTests/EngineTests.cs
@@ -0,0 +1,14 @@
+using PromotionEngine;
+using Xunit;
+
+namespace PromotionEngineTests
+{
+    public class EngineTests
+    {
+        [Fact]
+        public void ScenarioA()
+        {
+            var engine = new PromotionEngineV1();
+        }
+    }
+}
