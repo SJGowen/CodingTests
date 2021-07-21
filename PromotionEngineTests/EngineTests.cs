33dd7d2e0720aa8f0fa0ba59fa91ad097648b839
Stephen Gowen <sjgowen@gmail.com>
2021-07-21 11:34:07 +0100

    Failing second test has been added.

--- PromotionEngineTests/EngineTests.cs
+++ PromotionEngineTests/EngineTests.cs
@@ -17,6 +17,18 @@ namespace PromotionEngineTests
             Assert.Equal(100, total);
         }
 
+        [Fact]
+        public void ScenarioB()
+        {
+            var engine = new PromotionEngineV1();
+            StockWithTestData(engine);
+            engine.Add("A", 5);
+            engine.Add("B", 5);
+            engine.Add("C", 1);
+            var total = engine.CalculateTotal();
+            Assert.Equal(370, total);
+        }
+
         private void StockWithTestData(PromotionEngineV1 engine)
         {
             engine.AddStock("A", 50m);
